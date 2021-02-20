20c552b2d1180e56e7cfb2a1ddbcfda4d7d35c5c
Lupita <lupitarivera8899@gmail.com>
2021-02-20 15:51:00 -0600

    commit tree 91577dcc1410f6fa123a205116d6e3233089fb85

--- lib/nyc_api.rb
+++ lib/nyc_api.rb
@@ -1,7 +1,7 @@
 require 'net/http'
 require 'open-uri'
 require 'json'
- 
+
 class GetPrograms
 
   URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
@@ -14,5 +14,17 @@ class GetPrograms
 
 end
 
-programs = GetPrograms.new.get_programs
-puts programs
+def program_school
+  # we use the JSON library to parse the API response into nicely formatted JSON
+    programs = JSON.parse(self.get_programs)
+    programs.collect do |program|
+      program["agency"]  
+    end
+  end 
+  
+  programs = GetPrograms.new
+  puts programs.program_school.uniq 
+
+# programs = GetPrograms.new.get_programs
+# puts programs
+
